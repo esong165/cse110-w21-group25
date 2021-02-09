ef304b7cccbb131035c71bd90a6ce4e9a172608d
esong165 <esong165@gmail.com>
2021-02-09 10:08:37 -0800

    Add test js file

---
+++ source/test.js
@@ -0,0 +1,13 @@
+/**
+ * @file This file tests our JSDoc workflow.
+ */
+
+/**
+ * A function to add two numbers.
+ * @param {number} x - The first number to be added.
+ * @param {number} y - The second number to be added.
+ * @returns {number} - The sum of x and y.
+ */
+ function testFunc(x, y){
+    return x + y;
+ }
