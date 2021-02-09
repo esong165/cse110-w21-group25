95db5a846aee1bd2dd2404467a4e1b9fb6d72391
esong165 <esong165@gmail.com>
2021-02-08 23:17:02 -0800

    Added JSDoc

--- source/main.js
+++ source/main.js
@@ -3,8 +3,9 @@ btn.addEventListener('click', changeVisibility)
 
 /**
  * A function to change visibility of a popup.
+ * @param {Event} event - The event of the button being clicked.
  */
-function changeVisibility () {
+function changeVisibility (event) {
   const display = document.getElementById('popup')
   const vis = window.getComputedStyle(display).visibility
   if (vis === 'hidden') {
