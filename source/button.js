bacb107cc40619fc2e80e45fdaf016c26fc09aab
esong165 <esong165@gmail.com>
2021-02-10 11:22:14 -0800

    edited linter settings

--- source/button.js
+++ source/button.js
@@ -1,16 +1,16 @@
-const btn = document.getElementById('btn')
-btn.addEventListener('click', changeVisibility)
+const btn = document.getElementById('btn');
+btn.addEventListener('click', changeVisibility);
 
 /**
  * A function to change visibility of a popup.
  * @param {Event} event - The event of the button being clicked.
  */
-function changeVisibility (event) {
-  const display = document.getElementById('popup')
-  const vis = window.getComputedStyle(display).visibility
+function changeVisibility(event) {
+  const display = document.getElementById('popup');
+  const vis = window.getComputedStyle(display).visibility;
   if (vis === 'hidden') {
-    display.style.visibility = 'visible'
+    display.style.visibility = 'visible';
   } else {
-    display.style.visibility = 'hidden'
+    display.style.visibility = 'hidden';
   }
 }
