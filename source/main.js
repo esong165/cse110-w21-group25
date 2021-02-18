b4d486848dc8c96514b7122e5354293fda943476
bananahbatman <fasterthanlight1.1@gmail.com>
2021-02-17 16:35:39 -0800

    created selectTask, localStorage, and done

--- source/main.js
+++ source/main.js
@@ -1,40 +1,110 @@
 function manageTaskList() {
-    let taskContainer = document.getElementById('task-list');
+    let taskList = document.getElementById('task-list');
     let taskButton = document.getElementById('tasklist-btn');
-    let display = window.getComputedStyle(taskContainer).display;
+    let display = window.getComputedStyle(taskList).display;
+    let taskContainer = document.getElementById('tasks-container');
+    
+    //Currently this is the only way I can get populateTaskList() to work
+    // Note: Since i put populateTaskList here the selected task after a refresh only works when you press tasklist-btn
+    if(!(taskContainer.hasChildNodes())){
+        populateTaskList();
+    }
+    
     
     if(display === 'none') {
-        taskContainer.style.display = 'block';
+
+        taskList.style.display = 'block';
         taskButton.innerHTML = 'Close Task List';
     } else {
-        taskContainer.style.display = 'none';
+        taskList.style.display = 'none';
         taskButton.innerHTML = 'Open Task List';
     }
+}
+
+//populate tasklist with local storage
+function populateTaskList(){
+    let taskItemArr = JSON.parse(localStorage.getItem("taskItemArr"));
+    //creates new map if there is none
+    if (taskItemArr == null || taskItemArr.length == 0) {
+        
+        taskItemArr = [];
+        localStorage.setItem("taskItemArr", JSON.stringify(taskItemArr));
+
+        selectedTask =[];
+        selectedTask.push("Current Task: Default");
+        selectedTask.push("# of Pomos: 1");
+        localStorage.setItem("selectedTask", JSON.stringify(selectedTask));
+    }
+    else{
+
+        let selectedTask = JSON.parse(localStorage.getItem("selectedTask"));
+
+        document.getElementById("current-task").innerHTML = selectedTask[0];
+        document.getElementById("num-pomos").innerHTML = selectedTask[1];
+        //add all tasks from local storage
+        
+        for(let i = 0; i< taskItemArr.length; i++){
+            
+            let task = new TaskItem(taskItemArr[i][0], taskItemArr[i][1]);
+            task.id = taskItemArr[i][0];
+
+            // Get task item
+            let list = task.shadowRoot.children[0];
+
+            //Add select button functionality
+            list.children[2].addEventListener('click', function() {selectTask(task.id)});
+            
+            // Add remove button functionality
+            list.children[3].addEventListener('click', function() {removeTask(task.id)});
+
+            document.getElementById('tasks-container').appendChild(task);
+        }
+
+        
+    }
+    
 }
 
 function addTask(event) {
     event.preventDefault();
     let name = document.getElementById('new-task-name');
     let count = document.getElementById('new-task-count');
+    
+
     if(name.value === '' || document.getElementById(name.value) !== null) {
         alert('Invalid task input or task already added');
     }
     else{
-        let task = document.createElement('task-item');
+        //function() {selectTask(name.value)}, function() {removeTask(name.value)}
+        let task = new TaskItem(name.value, count.value);
         task.id = name.value;
-
+        
         // Get task item
         let list = task.shadowRoot.children[0];
 
-        // Set name of task
-        list.children[0].innerHTML += name.value + ', ';
+        //store task in local storage 
+        /*
+        let taskItemMap = JSON.parse(localStorage.getItem("taskItemMap"));
+        taskItemMap.set(name.value, count.value);
+        localStorage.setItem("taskItemMap", JSON.stringify(taskItemMap));
+        */
 
-        // Set estimated pomodoros
-        list.children[1].innerHTML += count.value;
+        //store task as array of array in local storage
+        let taskItemArr = JSON.parse(localStorage.getItem("taskItemArr"));
+        taskNameNum = [];
+        taskNameNum.push(name.value);
+        taskNameNum.push(count.value);
+
+        taskItemArr.push(taskNameNum);
+        localStorage.setItem("taskItemArr", JSON.stringify(taskItemArr));
+
+         //Add select button functionality
+         list.children[2].addEventListener('click', function() {selectTask(task.id)});
         
-        // Add remove button
-        list.children[2].addEventListener('click', function() {removeTask(task.id)});
+         // Add remove button functionality
+         list.children[3].addEventListener('click', function() {removeTask(task.id)});
 
+        //add task to list
         document.getElementById('tasks-container').appendChild(task);
 
         // Reset input fields
@@ -43,9 +113,67 @@ function addTask(event) {
     }
 }
 
-function removeTask(taskId){
-    console.log(taskId);
-    document.getElementById('tasks-container').removeChild(document.getElementById(taskId));
+//Done with Task 
+
+function doneTask(event){
+    event.preventDefault();
+    //Code here for statistics
+    taskStr = document.getElementById("current-task").innerHTML;
+    taskId = taskStr.substring(11, taskStr.length-2);
+    removeTask(taskId);
+
+    //reset tasks
+    document.getElementById("current-task").innerHTML ="Current Task: Default";
+    document.getElementById("num-pomos").innerHTML ="# of Pomos: 1";
+
 }
 
+
+function selectTask(taskId){
+    //choose one of the following
+    //change the button to be task 
+    //document.getElementById("tasklist-btn").innerHTML = taskId;
+    //change the current-task header
+    let task = document.getElementById(taskId);
+    let list = task.shadowRoot.children[0];
+
+    document.getElementById("current-task").innerHTML = list.children[0].innerHTML;
+    document.getElementById("num-pomos").innerHTML = list.children[1].innerHTML;
+    
+    //stores selected task in local storage
+    let selectedTask = [];
+    selectedTask.push(list.children[0].innerHTML);
+    selectedTask.push(list.children[1].innerHTML);
+    localStorage.setItem("selectedTask", JSON.stringify(selectedTask));
+}
+
+function removeTask(taskId){
+    if (document.getElementById("current-task").innerHTML == taskId)
+    {
+        document.getElementById("current-task").innerHTML = "Default Task";
+        
+    }
+    document.getElementById('tasks-container').removeChild(document.getElementById(taskId));
+
+    //remove task in local storage as map 
+    /*
+    let taskItemMap = JSON.parse(localStorage.getItem("taskItemMap"));
+    taskItemMap.delete(taskId);
+    localStorage.setItem("taskItemMap", JSON.stringify(taskItemMap));
+    */
+
+    //remove task as array of array in local storage
+    let taskItemArr = JSON.parse(localStorage.getItem("taskItemArr"));
+    taskItemArr = arrayRemove(taskItemArr, taskId);
+    localStorage.setItem("taskItemArr", JSON.stringify(taskItemArr));
+}
+
+function arrayRemove(arr, taskId) { 
+
+return arr.filter(function(ele){ 
+    return ele[0] != taskId; 
+});
+}
+    
+//populateTaskList(); Throws TaskItem not defined
 document.getElementById('tasklist-btn').addEventListener('click', manageTaskList);
--- source/task-item.js
+++ source/task-item.js
@@ -1,25 +1,40 @@
 class TaskItem extends HTMLElement {
-    constructor() {
+    constructor(taskName, pomoCount) {
         super();
+
 
         let shadow = this.attachShadow({mode: 'open'});
 
         let task = document.createElement('li');
         task.setAttribute('class', 'task');
 
+        //Set name of task
         let name = document.createElement('span');
         name.innerHTML = 'Task name: ';
+        name.innerHTML += taskName + ', ';
         task.appendChild(name);
 
+        //Set estimated pomodoros
         let count = document.createElement('span');
         count.innerHTML = 'Estimated pomodoros: ';
+        count.innerHTML += pomoCount;
         task.appendChild(count);
+
+        //select task button note: considering making this a radio option or making the text a clickable button
+        let selectTaskButton = document.createElement('button');
+        selectTaskButton.innerHTML = 'Select';
+
+        task.appendChild(selectTaskButton);
+        
 
         let removeTaskButton = document.createElement('button');
         //removeTaskButton.setAttribute('onclick', 'removeTask(taskId)');
         removeTaskButton.innerHTML = 'Remove';
+        
         task.appendChild(removeTaskButton);
         
+        
+
         shadow.appendChild(task);
 
         let style = document.createElement('style');
@@ -35,5 +50,4 @@ class TaskItem extends HTMLElement {
         */  
     }
 }
-
 customElements.define('task-item', TaskItem);
