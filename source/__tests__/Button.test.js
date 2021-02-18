5869e87218b21ea78c5614359e9c6916a27e7ec1
UCSDTOMPHAMDINH <59636279+UCSDTOMPHAMDINH@users.noreply.github.com>
2021-02-17 20:10:12 -0800

    test

---
+++ source/__tests__/Button.test.js
@@ -0,0 +1,13 @@
+
+test('Initial change', () => {
+	document.body.innerHTML = '<button id = "pomoButton">Start Pomo</button><div id ="time">25:00</div>';
+	require('../Button.js');
+
+	const theButton = document.getElementById('pomoButton');
+	const theTimer = document.getElementById('time');
+
+	Button('pomoButton','time');
+	theButton.click();
+	expect(theButton.textContent).toBe('Fail Task');
+	
+});
--- source/button.js
+++ source/button.js
@@ -1,16 +1,76 @@
-const btn = document.getElementById('btn');
-btn.addEventListener('click', changeVisibility);
+'use strict';
 
-/**
- * A function to change visibility of a popup.
- * @param {Event} event - The event of the button being clicked.
- */
-function changeVisibility(event) {
-  const display = document.getElementById('popup');
-  const vis = window.getComputedStyle(display).visibility;
-  if (vis === 'hidden') {
-    display.style.visibility = 'visible';
-  } else {
-    display.style.visibility = 'hidden';
-  }
+export class Button {
+	constructor(buttonId, clockId) {
+		let cycleCount = 0;
+		let onTask = false;
+		document.getElementById(buttonId).addEventListener('click', function() {
+		// Get the current time of the timer
+			const currentTime = document.getElementById(clockId).textContent;
+
+			// Timer hasnt begun yet
+			if (cycleCount === 0 && onTask === false) {
+				/* call the timer starter function **HERE** */
+
+				// Need to id to the one in the html
+				document.getElementById(buttonId).textContent = 'Fail Task';
+
+				onTask = true;
+			} else if (onTask === true) {
+				if (currentTime !== '0:00') {
+				/*
+				Set Timer interval to 25
+				**HERE**
+				*/
+
+					document.getElementById(buttonId).textContent = 'Start Pomo';
+				} else if (document.getElementById(buttonId).textContent === 'Start Pomo') {
+					document.getElementById(buttonId).textContent = 'Fail Task';
+				} else {
+					cycleCount++;
+					if (cycleCount % 3 === 0) {
+						/*
+						Set timer to 10 minutes
+						Start Countdown
+						**HERE**
+						*/
+						document.getElementById(buttonId).disabled = true;
+					} else {
+						/*
+						Set timer to 5 minutes
+						Start countdown
+						**HERE**
+						*/
+						document.getElementById(buttonId).disabled = true;
+					}
+				}
+
+				onTask = false;
+			} else if (onTask === false) {
+				/*
+				Set timer to 25 minutes
+				Start Countdown
+				**HERE**
+				*/
+			}
+		});
+
+		document.getElementById(clockId).addEventListener('change', function() {
+			const currentTime = document.getElementById(clockId).textContent;
+
+			if (currentTime === '0:00') {
+				if (onTask === false) {
+					document.getElementById(buttonId).textContent = 'Start Pomo';
+				} else {
+					if (cycleCount % 3 === 0) {
+						document.getElementById(buttonId).textContent = 'Start Long Break';
+					} else {
+						document.getElementById(buttonId).textContent = 'Start Short Break';
+					}
+
+					document.getElementById(buttonId).disabled = false;
+				}
+			}
+		});
+	}
 }
