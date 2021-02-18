d010d3849075e181736727b24993f7a1a0e2b19d
anbchi <77029692+anbchi@users.noreply.github.com>
2021-02-18 11:35:24 -0800

    Add files via upload

--- source/Timer/timer.js
+++ source/Timer/timer.js
@@ -1,44 +1,67 @@
+var myMinutes;
 var mySeconds;
 var intervalHandle;
+document.getElementById('startTimer').addEventListener('click', startCounter);
 
 function resetPage(){
-	document.getElementById("inputArea").style.display="none";		
+	document.getElementById("inputArea").style.display="block";		
+	document.getElementById("rules").style.display="block";
+	document.getElementById("startTimer").style.display="block";	
 }
 function tick(){
 	var timeDisplay=document.getElementById("time");
 		
-	var min=Math.floor(mySeconds/60);
-	var sec=mySeconds-(min*60);
-		
+	var min=myMinutes;
+	var sec=mySeconds;
+
 	if (sec < 10) {
 		sec="0"+sec;
+	}
+	if (min==0){
+		min="00";
+	}
+	else if (min < 10) {
+		min="0"+min;
 	}
 		
 	var message=min.toString()+":"+sec;
 		
 	timeDisplay.innerHTML=message;
 		
-	if(mySeconds===0){
-		alert("Done");
+	if(mySeconds==0 && myMinutes != 0)
+	{
+		myMinutes--;
+		mySeconds=60;
+	}
+	else if(mySeconds===0){
+		document.getElementById("alarm").play();
 		clearInterval(intervalHandle);
 		resetPage();
 	}
 	mySeconds--;
 }
+
+
 function startCounter(){
-	var myInput=document.getElementById("minutes").value;
-	if (isNaN(myInput)){
+	myMinutes = document.getElementById("minutes").value;
+	mySeconds = document.getElementById("seconds").value;
+	if (isNaN(myMinutes)){
 		alert("Type a valid number please");
 		return;
 	}
-	mySeconds=myInput*60;
+	if (isNaN(mySeconds)){
+		alert("Type a valid number please");
+		return;
+	}
 		
 	intervalHandle=setInterval(tick, 1000);
 		
+	document.getElementById("rules").style.display="none";
 	document.getElementById("inputArea").style.display="none";	
+	document.getElementById("startTimer").style.display="none";	
 }
 
-
+/*
 window.onload=function(){
 	var myInput=document.createElement("input");
 	myInput.setAttribute("type","text");
@@ -54,3 +77,4 @@ window.onload=function(){
 	document.getElementById("inputArea").appendChild(myInput);
 	document.getElementById("inputArea").appendChild(myButton);	
 }
+*/
