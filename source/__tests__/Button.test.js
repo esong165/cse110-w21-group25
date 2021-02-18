46935fd6a4810717c9153fd3adf677a880a1c68f
UCSDTOMPHAMDINH <59636279+UCSDTOMPHAMDINH@users.noreply.github.com>
2021-02-17 20:15:13 -0800

    Update Button.test.js

--- source/__tests__/Button.test.js
+++ source/__tests__/Button.test.js
@@ -1,7 +1,7 @@
 
 test('Initial change', () => {
 	document.body.innerHTML = '<button id = "pomoButton">Start Pomo</button><div id ="time">25:00</div>';
-	require('../button.js');
+	require('../Button.js');
 
 	const theButton = document.getElementById('pomoButton');
 	const theTimer = document.getElementById('time');
