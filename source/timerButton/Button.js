7ed3761a25072d9f7f91e2e518f9b2141b2a3284
UCSDTOMPHAMDINH <59636279+UCSDTOMPHAMDINH@users.noreply.github.com>
2021-02-17 18:31:26 -0800

    lints

--- source/timerButton/Button.js
+++ source/timerButton/Button.js
@@ -1,9 +1,9 @@
 'use strict';
 
 class Button {
-	constructor (buttonId, clockId) {
-		let cycleCount = 0
-		let onTask = false
+	constructor(buttonId, clockId) {
+		let cycleCount = 0;
+		let onTask = false;
 		document.getElementById(buttonId).addEventListener('click', function() {
 		// Get the current time of the timer
 			const currentTime = document.getElementById(clockId).textContent;
@@ -13,9 +13,9 @@ class Button {
 				/* call the timer starter function **HERE** */
 
 				// Need to id to the one in the html
-				document.getElementById(buttonId).textContent = 'Fail Task'
+				document.getElementById(buttonId).textContent = 'Fail Task';
 
-				onTask = true
+				onTask = true;
 			} else if (onTask === true) {
 				if (currentTime !== '0:00') {
 				/*
@@ -23,40 +23,40 @@ class Button {
 				**HERE**
 				*/
 
-				document.getElementById(buttonId).textContent = 'Start Pomo'
-			} else if (document.getElementById(buttonId).textContent === 'Start Pomo') {
-				document.getElementById(buttonId).textContent = 'Fail Task'
-			} else {
-				cycleCount++
-				if (cycleCount % 3 === 0) {
+					document.getElementById(buttonId).textContent = 'Start Pomo';
+				} else if (document.getElementById(buttonId).textContent === 'Start Pomo') {
+					document.getElementById(buttonId).textContent = 'Fail Task';
+				} else {
+					cycleCount++;
+					if (cycleCount % 3 === 0) {
+						/*
+						Set timer to 10 minutes
+						Start Countdown
+						**HERE**
+						*/
+						document.getElementById(buttonId).disabled = true;
+					} else {
+						/*
+						Set timer to 5 minutes
+						Start countdown
+						**HERE**
+						*/
+						document.getElementById(buttonId).disabled = true;
+					}
+				}
+
+				onTask = false;
+			} else if (onTask === false) {
 				/*
-				Set timer to 10 minutes
+				Set timer to 25 minutes
 				Start Countdown
 				**HERE**
 				*/
-				document.getElementById(buttonId).disabled = true
-			} else {
-				/*
-				Set timer to 5 minutes
-				Start countdown
-				**HERE**
-				*/
-				document.getElementById(buttonId).disabled = true
 			}
-		}
+		});
 
-				onTask = false
-		} else if (onTask === false) {
-			/*
-			Set timer to 25 minutes
-			Start Countdown
-			**HERE**
-			*/
-		}
-	})
-
-		document.getElementById(clockId).addEventListener('change', function () {
-			const currentTime = document.getElementById(clockId).textContent
+		document.getElementById(clockId).addEventListener('change', function() {
+			const currentTime = document.getElementById(clockId).textContent;
 
 			if (currentTime === '0:00') {
 				if (onTask === false) {
@@ -71,10 +71,10 @@ class Button {
 					document.getElementById(buttonId).disabled = false;
 				}
 			}
-		})
+		});
 	}
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	Button('timer-btn', 'time')
-})
+	Button('timer-btn', 'time');
+});
