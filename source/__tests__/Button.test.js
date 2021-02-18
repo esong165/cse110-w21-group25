13ed1ea16b14809d3abed12174a820ceb10b7b29
UCSDTOMPHAMDINH <59636279+UCSDTOMPHAMDINH@users.noreply.github.com>
2021-02-17 20:42:06 -0800

    Update Button.test.js

--- source/__tests__/Button.test.js
+++ source/__tests__/Button.test.js
@@ -1,4 +1,4 @@
-import Button from '../Button.js'
+import Button from '../Button.js';
 
 test('Initial change', () => {
 	document.body.innerHTML = '<button id = "pomoButton">Start Pomo</button><div id ="time">25:00</div>';
@@ -9,5 +9,17 @@ test('Initial change', () => {
 	const button = new Button('pomoButton','time');
 	theButton.click();
 	expect(theButton.textContent).toBe('Fail Task');
+	
+});
+
+test('Initial change', () => {
+	document.body.innerHTML = '<button id = "pomoButton">Start Pomo</button><div id ="time">25:00</div>';
+
+	const theButton = document.getElementById('pomoButton');
+	const theTimer = document.getElementById('time');
+
+	const button = new Button('pomoButton','time');
+	theTimer.textContent = '0:00';
+	expect(theButton.textContent).toBe('Short Break');
 	
 });
