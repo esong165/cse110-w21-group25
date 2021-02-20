733c1a07efa43b1e84251b76b44359c7cea8e767
UCSDTOMPHAMDINH <59636279+UCSDTOMPHAMDINH@users.noreply.github.com>
2021-02-19 20:10:10 -0800

    describe

--- source/__tests__/Button.test.js
+++ source/__tests__/Button.test.js
@@ -1,42 +1,45 @@
 import Button from '../Button.js';
 
-beforeEach(() => {
-	document.body.innerHTML = '<button id = "pomoButton">Start Pomo</button><div id ="time">25:00</div>';
+describe('button tests', () => {
+	let theButton;
+	let theTimer;
+	let button;
+	beforeEach(() => {
+		document.body.innerHTML = '<button id = "pomoButton">Start Pomo</button><div id ="time">25:00</div>';
+		theButton = document.getElementById('pomoButton');
+		theTimer = document.getElementById('time');
 
-	var theButton = document.getElementById('pomoButton');
-	var theTimer = document.getElementById('time');
+		button = new Button('pomoButton','time');
+ 	 });
 
-	var button = new Button('pomoButton','time');
-  });
-
-test('Initial change', () => {
+	test('Initial change', () => {
 	
-	theButton.click();
-	expect(theButton.textContent).toBe('Fail Task');
-	
-});
-
-test('ShortBreak', () => {
-
-	theButton.click();
-	theTimer.textContent = '0:00';
-	setTimeout(function(){
-		expect(theButton.textContent).toBe('Start Short Break');
-	}, 4000);
-	
-	
-});
-
-test('new Pomo', () => {
-
-	theButton.click();
-	theTimer.textContent = '0:00';
-	setTimeout(function(){
-		expect(theButton.textContent).toBe('Start Short Break');
 		theButton.click();
-		expect(theButton.textContent).toBe('Start Pomo');
-	}, 4000);
+		expect(theButton.textContent).toBe('Fail Task');
+	
+	});
 
+	test('ShortBreak', () => {
+
+		theButton.click();
+		theTimer.textContent = '0:00';
+		setTimeout(function(){
+			expect(theButton.textContent).toBe('Start Short Break');
+		}, 4000);
 	
 	
+	});
+
+	test('new Pomo', () => {
+
+		theButton.click();
+		theTimer.textContent = '0:00';
+		setTimeout(function(){
+			expect(theButton.textContent).toBe('Start Short Break');
+			theButton.click();
+			expect(theButton.textContent).toBe('Start Pomo');
+		}, 4000);
+
+	});
+
 });
