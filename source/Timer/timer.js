2ccacd71026ea320427ea56de228fdf76ace7375
anbchi <77029692+anbchi@users.noreply.github.com>
2021-02-20 13:01:39 -0800

    Update timer.js

--- source/Timer/timer.js
+++ source/Timer/timer.js
@@ -1,33 +1,34 @@
 let countDown;
+let intervalHandle;
 
 export default class CountDown {
 	constructor() {
-		this.myMinutes = document.getElementById("minutes").value;
-		this.mySeconds = document.getElementById("seconds").value;
-		this.input = document.getElementById("input-area").style.display = 'block';
-		this.rules = document.getElementById("rules").style.display = 'block';
-		this.start = document.getElementById("start-timer").style.display = 'block';
+		this.myMinutes = document.getElementById('minutes').value;
+		this.mySeconds = document.getElementById('seconds').value;
+		this.input = document.getElementById('input-area').style.display = 'block';
+		this.rules = document.getElementById('rules').style.display = 'block';
+		this.start = document.getElementById('start-timer').style.display = 'block';
 	}
 
 	set tick() {
 		const timeDisplay = document.getElementById('time-display');
 		if (this.mySeconds < 10) {
-			this.mySeconds = "0" + this.mySeconds;
+			this.mySeconds = '0' + this.mySeconds;
 		}
 		if (this.myMinutes == 0) {
-			this.myMinutes = "00";
+			this.myMinutes = '00';
 		}
 		else if (this.myMinutes < 10) {
-			this.myMinutes = "0" + this.myMinutes;
+			this.myMinutes = '0' + this.myMinutes;
 		}
-		var message = this.myMinutes.toString() + ":" + this.mySeconds;
+		var message = this.myMinutes.toString() + ':' + this.mySeconds;
 		timeDisplay.innerHTML = message;
 		if(this.mySeconds === 0 && this.myMinutes !==0) 
 			this.myMinutes--;
-			this.mySeconds=60;
+			this.mySeconds = 60;
 		}
 		else if(this.mySeconds === 0) 
-			document.getElementById("alarm").play();
+			document.getElementById('alarm').play();
 			clearInterval(intervalHandle);
 			resetPage();
 		}
@@ -36,14 +37,14 @@ export default class CountDown {
 
 	set startCounter() {
 		if (isNaN(this.myMinutes)) {
-			alert("Type a valid number please");
+			alert('Type a valid number please');
 			return;
 		}
 		if (isNaN(this.mySeconds)) {
-			alert("Type a valid number please");
+			alert('Type a valid number please');
 			return;
 		}
-		let intervalHandle = setInterval(tick, 1000);
+		intervalHandle = setInterval(tick, 1000);
 		this.input = 'none';
 		this.rules = 'none';
 		this.start = 'none';
