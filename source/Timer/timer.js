0dacee41c6fc343bb40c54a8e25f39e02eb2a420
anbchi <77029692+anbchi@users.noreply.github.com>
2021-02-20 12:58:48 -0800

    Update timer.js

--- source/Timer/timer.js
+++ source/Timer/timer.js
@@ -1,62 +1,54 @@
-var myMinutes;
-var mySeconds;
-var intervalHandle;
-document.getElementById('start-timer').addEventListener('click', startCounter);
+let countDown;
 
-function resetPage(){
-	document.getElementById("input-area").style.display="block";		
-	document.getElementById("rules").style.display="block";
-	document.getElementById("start-timer").style.display="block";	
-}
-function tick(){
-	var timeDisplay=document.getElementById("time-display");
-		
-	var min=myMinutes;
-	var sec=mySeconds;
+export default class CountDown {
+	constructor() {
+		this.myMinutes = document.getElementById("minutes").value;
+		this.mySeconds = document.getElementById("seconds").value;
+		this.input = document.getElementById("input-area").style.display = 'block';
+		this.rules = document.getElementById("rules").style.display = 'block';
+		this.start = document.getElementById("start-timer").style.display = 'block';
+	}
 
-	if (sec < 10) {
-		sec="0"+sec;
+	set tick() {
+		const timeDisplay = document.getElementById('time-display');
+		if (this.mySeconds < 10) {
+			this.mySeconds = "0" + this.mySeconds;
+		}
+		if (this.myMinutes == 0) {
+			this.myMinutes = "00";
+		}
+		else if (this.myMinutes < 10) {
+			this.myMinutes = "0" + this.myMinutes;
+		}
+		var message = this.myMinutes.toString() + ":" + this.mySeconds;
+		timeDisplay.innerHTML = message;
+		if(this.mySeconds === 0 && this.myMinutes !==0) 
+			this.myMinutes--;
+			this.mySeconds=60;
+		}
+		else if(this.mySeconds === 0) 
+			document.getElementById("alarm").play();
+			clearInterval(intervalHandle);
+			resetPage();
+		}
+		this.mySeconds--;
 	}
-	if (min==0){
-		min="00";
-	}
-	else if (min < 10) {
-		min="0"+min;
-	}
-		
-	var message=min.toString()+":"+sec;
-		
-	timeDisplay.innerHTML=message;
-		
-	if(mySeconds==0 && myMinutes != 0)
-	{
-		myMinutes--;
-		mySeconds=60;
-	}
-	else if(mySeconds===0){
-		document.getElementById("alarm").play();
-		clearInterval(intervalHandle);
-		resetPage();
-	}
-	mySeconds--;
-}
 
-
-function startCounter(){
-	myMinutes = document.getElementById("minutes").value;
-	mySeconds = document.getElementById("seconds").value;
-	if (isNaN(myMinutes)){
-		alert("Type a valid number please");
-		return;
+	set startCounter() {
+		if (isNaN(this.myMinutes)) {
+			alert("Type a valid number please");
+			return;
+		}
+		if (isNaN(this.mySeconds)) {
+			alert("Type a valid number please");
+			return;
+		}
+		let intervalHandle = setInterval(tick, 1000);
+		this.input = 'none';
+		this.rules = 'none';
+		this.start = 'none';
 	}
-	if (isNaN(mySeconds)){
-		alert("Type a valid number please");
-		return;
-	}
-		
-	intervalHandle=setInterval(tick, 1000);
-		
-	document.getElementById("rules").style.display="none";
-	document.getElementById("input-area").style.display="none";	
-	document.getElementById("start-timer").style.display="none";	
-}
+};
+document.addEventListener('DOMContentLoaded', () => {
+	countDown = new CountDown();
+});
