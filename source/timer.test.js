40bd292a73a26accfbc25b72de70fcc8169d0239
UCSDTOMPHAMDINH <59636279+UCSDTOMPHAMDINH@users.noreply.github.com>
2021-02-25 12:11:56 -0800

    add missing test

--- source/timer.test.js
+++ source/timer.test.js
@@ -3,3 +3,7 @@ import Timer from './timer';
 test('works', () => {
 	expect(Timer.$format((10 * 60 + 31.1) * 1000)).toBe('10:31');
 });
+
+test('large timer format', () => {
+    expect(Timer.$format((100000000000 * 60 + 29.9) * 1000)).toBe('40:29');
+});
