dfcc1093dd221ab9bc6ae881ef029a07958bea0f
esong165 <esong165@gmail.com>
2021-03-05 23:11:47 -0800

    Revised test format

--- cypress/integration/tasklist-tests.js
+++ cypress/integration/tasklist-tests.js
@@ -8,14 +8,10 @@ describe('Tasklist Tests', () => {
 		cy.get('#new-task-name').clear().type('Simple Task');
 		cy.get('#new-task-count').clear().type('3');
 		cy.get('#add-task-btn').click();
-		cy.get('#tasks-container').then($el => { expect($el.$tasks[0]).to.eq('Simple Task') });
-		cy.get('#tasks-container').then($el => { expect($el.$tasks[1]).to.have.value(3) });
-		cy.get('#tasks-container').then($el => { expect($el.$tasks[2]).to.have.value(0) });
-
-		cy.get('#tasks-container').then($el => 
-			{ expect($el.children[0].shadowRoot[0].children[0].children[0].innerHTML).to.eq('Simple Task') });
-		cy.get('#tasks-container').then($el => 
-			{ expect($el.children[0].shadowRoot[0].children[0].children[1].innerHTML).to.have.value(3) });
-		cy.get('#tasks-container').then($el => { expect($el.children[0].currPomos).to.have.value(0) });
+		cy.get('#tasks-container').should('have.length', 1);
+		cy.get('#tasks-container', 'includeShadowDom').children('#Simple\ Task').as('task');
+		cy.get('@task').shadow().children().first().children().eq(0).then($el => {
+			expect($el).to.eq('Simple Task');
+		});
 	});
 });
