962daf35c950be15fbae33cf869e6518e989791e
esong165 <54968828+esong165@users.noreply.github.com>
2021-03-06 12:02:27 -0800

    Update task-item.js

--- source/task-item.js
+++ source/task-item.js
@@ -1,7 +1,7 @@
 /**
  * A class representing a task item.
  */
- export default class TaskItem extends HTMLElement {
+export default class TaskItem extends HTMLElement {
 	/**
 	 * Constructs a task item using the input task name and estimated pomodoro count.
 	 * Sets the current pomodoro count to 0.
