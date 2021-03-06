5109b0f37cb579078c4fa4e4ab69f77fc0b99621
esong165 <esong165@gmail.com>
2021-03-05 22:52:55 -0800

    Attempted fix of add tasks test

--- cypress/integration/tasklist-tests.js
+++ cypress/integration/tasklist-tests.js
@@ -11,11 +11,11 @@ describe('Tasklist Tests', () => {
 		cy.get('#tasks-container').then($el => { expect($el).$tasks[0].to.eq('Simple Task') });
 		cy.get('#tasks-container').then($el => { expect($el).$tasks[1].to.have.value(3) });
 		cy.get('#tasks-container').then($el => { expect($el).$tasks[2].to.have.value(0) });
-		
-		cy.get('#tasks-container').children[0].shadowRoot[0]
-			.children[0].children[0].innerHTML.then($el => { expect($el).to.eq('Simple Task') });
-		cy.get('#tasks-container').children[0].shadowRoot[0]
-			.children[0].children[1].innerHTML.then($el => { expect($el).to.have.value(3) });
-		cy.get('#tasks-container').children[0].currPomos.then($el => { expect($el).to.have.value(0) });
+
+		cy.get('#tasks-container').then($el => 
+			{ expect($el.children[0].shadowRoot[0].children[0].children[0].innerHTML).to.eq('Simple Task') });
+		cy.get('#tasks-container').then($el => 
+			{ expect($el.children[0].shadowRoot[0].children[0].children[1].innerHTML).to.have.value(3) });
+		cy.get('#tasks-container').then($el => { expect($el.children[0].currPomos).to.have.value(0) });
 	});
 });
