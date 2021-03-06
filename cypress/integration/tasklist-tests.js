8d6381e151cf56abe503a6994a78ccc30171c607
esong165 <esong165@gmail.com>
2021-03-05 22:32:58 -0800

    Edited add task test

--- cypress/integration/tasklist-tests.js
+++ cypress/integration/tasklist-tests.js
@@ -8,7 +8,7 @@ describe('Tasklist Tests', () => {
 		cy.get('#new-task-name').clear().type('Simple Task');
 		cy.get('#new-task-count').clear().type('3');
 		cy.get('#add-task-btn').click();
-		cy.get('#Simple Task').then($el => { expect($el).to.eq(cy.get('tasks-container').children[0]) });
+		cy.get('Simple Task').then($el => { expect($el).to.eq(cy.get('tasks-container').children[0]) });
 		cy.get('#tasks-container').then($el => { expect($el).$tasks[0].to.eq('Simple Task') });
 		cy.get('#tasks-container').then($el => { expect($el).$tasks[1].to.have.value(3) });
 		cy.get('#tasks-container').then($el => { expect($el).$tasks[2].to.have.value(0) });
