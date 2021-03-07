94adf8615d8f5d1a924f81ff75b0d529ce7f93da
Ainesh Arumugam <ainesh01@gmail.com>
2021-03-06 17:26:34 -0800

    added total tasks and avg pomos

--- source/statlist.js
+++ source/statlist.js
@@ -1,16 +1,19 @@
 import StatItem from './stat-item.js';
 
 export default class Statlist extends HTMLUListElement {
+
 	constructor() {
 		super();
-
+		let totalTasks = 0;
+		let totalPomos = 0;
+		let avgPomos = 0;
 		this.$stats = [];
 
 		const statItemArr = JSON.parse(localStorage.getItem('statItemArr'));
 		if (statItemArr !== null) {
 			this.$stats = statItemArr;
 		}
-
+ 
 		for (const stat of this.$stats) {
 			const current = new StatItem(stat[0], stat[1]);
 
@@ -30,6 +33,9 @@ export default class Statlist extends HTMLUListElement {
 		const statArray = [];
 		statArray.push(name);
 		statArray.push(count);
+		totalTasks++;
+		totalPomos+= count;
+		avgPomos = totalTasks/totalStat;
 
 		if (!JSON.stringify(statItemArr).includes(JSON.stringify(statArray))) {
 			statItemArr.push(statArray);
@@ -40,6 +46,9 @@ export default class Statlist extends HTMLUListElement {
 		}
 
 		localStorage.setItem('statItemArr', JSON.stringify(statItemArr));
+		localStorage.setItem('totalTasks', JSON.stringify(totalTasks));
+		localStorage.setItem('totalPomos', JSON.stringify(totalPomos));
+		localStorage.setItem('avgPomos', JSON.stringify(avgPomos));
 	}
 }
 customElements.define('stat-list', Statlist, { extends: 'ul' });
