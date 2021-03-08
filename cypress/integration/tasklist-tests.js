f3eb53209b5f07cf60df75750a9d74967919e436
esong165 <esong165@gmail.com>
2021-03-07 20:23:06 -0800

    Edited tests again

--- cypress/integration/tasklist-tests.js
+++ cypress/integration/tasklist-tests.js
@@ -31,7 +31,8 @@ describe('Tasklist Tests', () => {
 		cy.get('#new-task-name').clear().type('New Task');
 		cy.get('#new-task-count').clear().type('1');
 		cy.get('#add-task-btn').click();
-		cy.get('#tasks-container').children().get(0).click();
+		cy.get('#tasks-container').children().eq(0).as('task');
+		cy.get('@task').children().eq(2).click();
 		cy.get('#tasks-container').then($el => {
 			expect($el.get(0).$selected[0]).to.eq('New Task');
 			expect($el.get(0).$selected[1]).to.eq('1');
@@ -45,7 +46,7 @@ describe('Tasklist Tests', () => {
 		cy.get('#new-task-name').clear().type('Task');
 		cy.get('#new-task-count').clear().type('3');
 		cy.get('#add-task-btn').click();
-		cy.get('#tasks-container').children().get(0).children().eq(4).click();
+		cy.get('#tasks-container').children().eq(0).get(0).children().eq(4).click();
 		cy.get('#tasks-container').then($el => { expect($el.get(0).$tasks.length).to.eq(0); });
 		cy.get('#tasks-container').should('have.length', 0);
 	});
@@ -58,8 +59,8 @@ describe('Tasklist Tests', () => {
 		cy.get('#new-task-name').clear().type('Second Task');
 		cy.get('#new-task-count').clear().type('2');
 		cy.get('#add-task-btn').click();
-		cy.get('#tasks-container').children().get(0).children().eq(0).click();
-		cy.get('#tasks-container').children().get(0).children().eq(4).click();
+		cy.get('#tasks-container').children().eq(0).get(0).children().eq(0).click();
+		cy.get('#tasks-container').children().eq(0).get(0).children().eq(4).click();
 		cy.get('#tasks-container').should('have.length', 1);
 		cy.get('#tasks-container').then($el => {
 			expect($el.$selected[0]).to.eq('Second Task'); 
