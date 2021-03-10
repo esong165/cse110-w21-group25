fb9dc7e024f98f3ea61035680f739e9f42ede205
esong165 <esong165@gmail.com>
2021-03-10 09:28:53 -0800

    Final test fix (hopefully)

--- cypress/integration/tasklist-tests.js
+++ cypress/integration/tasklist-tests.js
@@ -63,7 +63,7 @@ describe('Tasklist Tests', () => {
 		cy.get('#add-task-button').click();
 		cy.get('#tasks-container').children().eq(0).shadow().children().eq(1).children().eq(0).children().eq(0).click();
 		cy.get('#task-list-button').click();
-		cy.get('#tasks-container').children().eq(0).shadow().children().eq(1).children().eq(2).click();
+		cy.get('#tasks-container').children().eq(0).shadow().children().eq(1).children().eq(1).click();
 		cy.get('#tasks-container').children().should('have.length', 1);
 		cy.get('#tasks-container').then($el => {
 			expect($el.get(0).$selected[0]).to.eq('_Second Task'); 
