5371fdf608d86bb610ebb56b9e12950cbfd9c59c
esong165 <esong165@gmail.com>
2021-03-14 14:03:23 -0700

    Added Notification tests

--- cypress/integration/timer-tests.js
+++ cypress/integration/timer-tests.js
@@ -57,4 +57,95 @@ describe('Timer tests', () => {
 		longBreak();
 		pomo('Long break in 3 pomos.');
 	});
+
+	it('Notification support', () => {
+		cy.window().should('have.property', 'Notification').should('be.a', 'function');
+	});
+
+	it('No notification support', () => {
+		cy.visit('/source/index.html', {
+			onBeforeLoad(window) {
+				delete window.Notification;
+			}
+		});
+		cy.window().should(window => {
+			window.app.settings.pomoDuration = 1000;
+			window.app.timer.notifySettingsChanged();
+		});
+		cy.on('window:alert', cy.stub().as('alerted'));
+		cy.get('#timer-button').click();
+		cy.wait(1000);
+		cy.get('@alerted').should('have.been.calledOnce').and('have.been.calledWith',
+			'This browser does not support desktop notification.');
+	});
+
+	it('Test notifications when permission is granted', () => {
+		cy.visit('/source/index.html', {
+			onBeforeLoad(window) {
+				cy.stub(window.Notification, 'permission', 'granted');
+				cy.stub(window, 'Notification').as('Notification');
+			}
+		});
+		cy.window().should(window => {
+			window.app.settings.pomoDuration = 1000;
+			window.app.settings.shortBreakDuration = 1000;
+			window.app.settings.longBreakDuration = 1000;
+			window.app.timer.notifySettingsChanged();
+		});
+		cy.get('#timer-button').click();
+		cy.wait(1000);
+		cy.get('@Notification').should('have.been.calledWithNew')
+			.and('have.been.calledWithExactly', 'Time\'s up! Start your short break now.');
+		cy.get('#timer-button').click();
+		cy.wait(1000);
+		cy.get('@Notification').should('have.been.calledWithNew')
+			.and('have.been.calledWithExactly', 'Time\'s up! Start your work session now.');
+		cy.window().should(window => {
+			window.app.timer.$cycle = 6;
+			window.app.timer.notifySettingsChanged();
+		});
+		cy.get('#timer-button').click();
+		cy.wait(1000);
+		cy.get('@Notification').should('have.been.calledWithNew')
+			.and('have.been.calledWithExactly', 'Time\'s up! Start your long break now.');
+		cy.get('#timer-button').click();
+		cy.wait(1000);
+		cy.get('@Notification').should('have.been.calledWithNew')
+			.and('have.been.calledWithExactly', 'Time\'s up! Start your work session now.');
+	});
+
+	it('Test asking for permission, shows notification if permission is granted', () => {
+		cy.visit('/source/index.html', {
+			onBeforeLoad(window) {
+				cy.stub(window.Notification, 'permission', 'unknown');
+				cy.stub(win.Notification, 'requestPermission').resolves('granted').as('ask');
+				cy.stub(window, 'Notification').as('Notification');
+			}
+		});
+		cy.window().should(window => {
+			window.app.settings.pomoDuration = 1000;
+			window.app.timer.notifySettingsChanged();
+		});
+		cy.get('#timer-button').click();
+		cy.wait(1000);
+		cy.get('@ask').should('have.been.calledOnce')
+			.and('have.been.calledBefore', cy.get('@Notification'));
+	});
+
+	it('No notification if permission has been denied', () => {
+		cy.visit('/source/index.html', {
+			onBeforeLoad(window) {
+				cy.stub(window.Notification, 'permission', 'denied');
+				cy.stub(win.Notification, 'requestPermission').resolves('denied').as('ask');
+				cy.stub(window, 'Notification').as('Notification');
+			}
+		});
+		cy.window().should(window => {
+			window.app.settings.pomoDuration = 1000;
+			window.app.timer.notifySettingsChanged();
+		});
+		cy.get('#timer-button').click();
+		cy.wait(1000);
+		cy.get('@Notification').should('not.have.been.called');
+	});
 });
