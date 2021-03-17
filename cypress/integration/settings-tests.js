5f55f739221ccc11508f2e72f84f41bf627b0695
Yuchen Jing <y5jing@ucsd.edu>
2021-03-16 19:41:08 -0700

    Fix: Cypress tests can't click with `display: none`

--- cypress/integration/settings-tests.js
+++ cypress/integration/settings-tests.js
@@ -8,17 +8,25 @@ describe('Settings Tests', () => {
 			window.app.timer.notifySettingsChanged();
 		});
 		cy.get('#current-task').should('have.text', 'No Current Task');
+		cy.get('#settings-button').click();
 		cy.get('#pomo-duration').select('20:00');
 		cy.get('#short-break-duration').select('10:00');
 		cy.get('#long-break-duration').select('30:00');
-		cy.get('#show-seconds').uncheck();
+		cy.get('#show-seconds-label').click();
 		cy.get('#alarm-sound').select('Alarm');
 		cy.get('#volume-slider').invoke('val', 0).trigger('input');
 	});
 
 	it('Reset to defaults', () => {
+		let startAndWait = ms => {
+			cy.get('#timer-button').click();
+			cy.wait(ms);
+		};
+		cy.get('#home-button').click();
 		cy.get('#time-remaining').should('have.text', '20');
+		cy.get('#settings-button').click();
 		cy.get('#reset-settings').click();
+
 		cy.get('#pomo-duration').find('option:selected').should('have.text', '25:00');
 		cy.get('#short-break-duration').find('option:selected').should('have.text', '5:00');
 		cy.get('#long-break-duration').find('option:selected').should('have.text', '15:00');
@@ -26,26 +34,25 @@ describe('Settings Tests', () => {
 		cy.get('#alarm-sound').find('option:selected').should('have.text', 'Beeping');
 		cy.get('#volume-slider').should('have.value', '100');
 
-		cy.get('#alarm').invoke('attr', 'src').should('eq', 'sounds/beeping.mp3');
-		cy.get('#alarm').invoke('attr', 'volume').should('eq', '100');
+		cy.get('#home-button').click();
+
+		cy.get('#alarm').should('have.prop', 'src').then(src => {
+			expect(src).to.contain('sounds/beeping.mp3');
+		});
+		cy.get('#alarm').should('have.prop', 'volume', 1);
 
 		cy.get('#time-remaining').should('have.text', '25:00');
-		cy.get('#timer-button').click();
-		cy.wait(3500);
+		startAndWait(3300);
 		cy.get('#time-remaining').should('have.text', '5:00');
-		cy.get('#timer-button').click();
-		cy.wait(1500);
+		startAndWait(1300);
 
-		cy.get('#timer-button').click();
-		cy.wait(3300);
-		cy.get('#timer-button').click();
-		cy.wait(1300);
-		cy.get('#timer-button').click();
-		cy.wait(3300);
-		cy.get('#timer-button').click();
-		cy.wait(1300);
-		cy.get('#timer-button').click();
-		cy.wait(3300);
+		startAndWait(3300);
+		startAndWait(1300);
+
+		startAndWait(3300);
+		startAndWait(1300);
+
+		startAndWait(3300);
 		cy.get('#time-remaining').should('have.text', '15:00');
 	});
 });
