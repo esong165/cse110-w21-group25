abd5d609ded1fffcdfb1508b003c4bb036b54b04
Ainesh Arumugam <ainesh01@gmail.com>
2021-03-17 17:16:54 -0700

    more comment changes

--- source/timer.js
+++ source/timer.js
@@ -98,7 +98,7 @@ export default class Timer {
 	}
 
 	/**
-	 * Formats the given time to the form mm.
+	 * Formats the given time to the form mm from mm:ss.
 	 * @param {Number} time - the time, in miliseconds, to format
 	 */
 	static $formatShort(time) {
