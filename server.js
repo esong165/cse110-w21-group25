ea2133f3b9f1993477a5115ece36ca174ec44b4e
DuckDuckWhaleUCSD <DuckDuckWhaleUCSD@github.com>
2021-03-18 23:38:30 -0700

    Deploy: remove tests and deploy to GitHub

--- server.js
+++
@@ -1,7 +0,0 @@
-const server = require('server');
-const { get, post } = server.router;
-
-// Handle requests to the url "/" ( http://localhost:3000/ )
-server([
-  get('/', ctx => 'Hello world!')
-]);
